4a748a2891d56b2edc154f68cb59b629c08bd2d5
asanbayev <asanbayev@kolesa.kz>
2023-02-26 10:38:12 +0600

    TTT-1(Номер задачи) Оставить замечания(Описание задачи)
    Оставил замечания по коду в комментариях(Что было сделано)

--- app/src/main/java/lovegoodbs/tictactoe/SettingsActivity.kt
+++ app/src/main/java/lovegoodbs/tictactoe/SettingsActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SettingsActivity : AppCompatActivity() {
+    //добавть пустую строку
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
